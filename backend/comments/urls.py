bfc1a766d4032ed92f30853a3c780fca929a1852
IN THE DOOR 2 <kristoffdixon19@gmail.com>
2023-04-01 23:11:11 -0400

    Created a GET endpoint and POST endpoint that accomplishes user story + Updated urls file to match the views path

--- backend/comments/urls.py
+++ backend/comments/urls.py
@@ -1,7 +1,5 @@
-from django.urls import path, include
+from django.urls import path
 from comments import views
-
-# <<<<<<<<<<<<<<<<< EXAMPLE FOR STARTER CODE USE <<<<<<<<<<<<<<<<<
 
 urlpatterns = [
     path('', views.user_comments),
