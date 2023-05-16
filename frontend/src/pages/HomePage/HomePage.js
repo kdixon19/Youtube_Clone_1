f234a2a9b6e785b088c2e2db0964583712fe87df
IN THE DOOR 2 <kristoffdixon19@gmail.com>
2023-05-16 14:48:59 -0400

    Created a useVideoPush hook

--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -1,43 +1,64 @@
+// General Imports
+import { Routes, Route, useNavigate } from "react-router-dom";
 import React from "react";
 import { useEffect, useState } from "react";
-import useAuth from "../../hooks/useAuth";
-
 import axios from "axios";
-import { DATA } from "../../localData";
 
-const HomePage = () => {
-  // The "user" value from this Hook contains the decoded logged in user information (username, first name, id)
-  // The "token" value is the JWT token that you will send in the header of any request requiring authentication
-  //TODO: Add an AddCars Page to add a car for a logged in user's garage
-  const [user, token] = useAuth();
-  const [cars, setCars] = useState([]);
+// Page Imports
+import VideoPage from "../VideoPage/VideoPage";
+import SearchResultsPage from "../SearchPage/SearchPage";
+
+// Utility Imports
+import DATA from "../HomePage/localData"
+
+// Component Imports
+import SearchBar from "../../components/SearchBar/SearchBar";
+
+function YouTubePage() {
+  const [videoResults, setVideoResults] = useState([DATA]);
+  const navigate = useNavigate();
+
+  if (process.env.NODE_ENV !== "production") {
+    console.log("It's working!");
+  }
 
   useEffect(() => {
-    const fetchCars = async () => {
-      try {
-        let response = await axios.get("http://127.0.0.1:8000/api/cars/", {
-          headers: {
-            Authorization: "Bearer " + token,
+    fetchResults("starwars");
+  }, []);
+
+  const fetchResults = async (searchTerm) => {
+    try {
+      let response = await axios.get(
+        `https://www.googleapis.com/youtube/v3/search`,
+        {
+          params: {
+            q: searchTerm,
+            key: process.env.REACT_APP_YT_API_KEY,
+            part: "snippet",
+            type: "video",
+            maxResults: 5,
           },
-        });
-        setCars(response.data);
-      } catch (error) {
-        console.log(error.response.data);
-      }
-    };
-    fetchCars();
-  }, [token]);
+        }
+      );
+      setVideoResults(response.data.items);
+      navigate("/");
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <div className="container">
-      <h1>Home Page for {user.username}!</h1>
-      {cars &&
-        cars.map((car) => (
-          <p key={car.id}>
-            {car.year} {car.model} {car.make}
-          </p>
-        ))}
+      <SearchBar handleSearch={fetchResults} />
+      <Routes>
+        <Route
+          path="/"
+          element={<SearchResultsPage results={videoResults} />}
+        />
+        <Route path="/:videoId" element={<VideoPage />} />
+      </Routes>
     </div>
   );
-};
+}
 
-export default HomePage;
+export default YouTubePage;
